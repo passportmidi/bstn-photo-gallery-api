c5f2a4c81dcb4de39f43c39c3b183472b7578cb5
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-01 23:23:24 -0400

    add comments post method

--- routes/photos.js
+++ routes/photos.js
@@ -1,6 +1,8 @@
 import express from "express";
 import photos from "../data/photos.json" with {type: "json"};
 import comments from "../data/comments.json" with {type: "json"};
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
 
 const router = express.Router();
 
@@ -20,13 +22,42 @@ router.route("/:id").get((req, res) => {
   res.json(photo);
 });
 
-router.route("/:id/comments").get((req, res) => {
-  const id = req.params.id;
-  const photoComments = comments.filter((c) => c.id === id);
-  const commentsArray = photoComments[0].comments;
-  console.log(commentsArray);
-
-  res.json(commentsArray);
-});
+router
+  .route("/:id/comments")
+  .get((req, res) => {
+    const id = req.params.id;
+    const commentsArray = comments[id];
+    res.json(commentsArray);
+  })
+  .post((req, res) => {
+    try {
+      const { name, comment } = req.body;
+      if (!name) {
+        throw new Error("Name cannot be empty");
+      }
+      else if (!comment) {
+        throw new Error("Comment cannot be empty");
+      }
+      else {
+        const newComment = {
+          name,
+          comment,
+          id: uuidv4(),
+          timestamp: Date.now(),
+        };
+        const id = req.params.id;
+        comments[id].push(newComment);
+  
+        // write updated comments to file
+        const json = JSON.stringify(comments);
+        fs.writeFileSync("./data/comments.json", json);
+  
+        res.status(201).json(comments[id]);
+      }
+    } catch (error) {
+      // TODO: will return a 400 error code if name, or comment are not included in the object, or if name, or comment are not strings
+      res.status(400).send(`${error}`);
+    }
+  });
 
 export default router;
